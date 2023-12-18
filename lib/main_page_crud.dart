d957db8958e0bf720e54d556f6520596ae87ed38
AbuRasel9 <aburaselahamed99@gmail.com>
2023-12-19 00:20:18 +0600

    ui done

--- lib/main_page_crud.dart
+++ lib/main_page_crud.dart
@@ -8,6 +8,10 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final namecontroller=TextEditingController();
+  final emailcontroller=TextEditingController();
+  final addresscontroller=TextEditingController();
+  final phonecontroller=TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,12 +20,81 @@ class _MainPageState extends State<MainPage> {
       ),
       body: Padding(
         padding: EdgeInsets.all(20),
-        child: Column(
-          children: [
+        child: Form(
+          child: Column(
+            children: [
+              
+              TextFormField(
+                controller: namecontroller,
+                validator: (String ?value){
+                  if(value?.isEmpty ?? true){
+                    return "Enter name";
+                  }
+                  return null;
+                },
+                
+                decoration: buildInputDecoration("Enter name"),
+              ),
+              SizedBox(height: 15,),
+              TextFormField(
+                controller: namecontroller,
+                validator: (String ?value){
+                  if(value?.isEmpty ?? true){
+                    return "Enter name";
+                  }
+                  return null;
+                },
+                
+                decoration: buildInputDecoration("Enter name"),
+              ),
+              SizedBox(height: 15,),
+              TextFormField(
+                controller: namecontroller,
+                validator: (String ?value){
+                  if(value?.isEmpty ?? true){
+                    return "Enter name";
+                  }
+                  return null;
+                },
+                
+                decoration: buildInputDecoration("Enter name"),
+              ),
+              SizedBox(height: 15,),
+              TextFormField(
+                controller: namecontroller,
+                validator: (String ?value){
+                  if(value?.isEmpty ?? true){
+                    return "Enter name";
+                  }
+                  return null;
+                },
+                
+                decoration: buildInputDecoration("Enter name"),
+              ),
+              SizedBox(height: 25,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(onPressed: (){}, child: Text("add")),
+                  ElevatedButton(onPressed: (){}, child: Text("update"))
+                ],
+              ),
+              Expanded(child: ListView())
+              
 
-          ],
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  InputDecoration buildInputDecoration(String text) {
+    return InputDecoration(
+                fillColor: Colors.greenAccent,
+                filled: true,
+                hintText: text,
+                
+              );
   }
 }
